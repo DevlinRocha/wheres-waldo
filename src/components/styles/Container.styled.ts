a9097a30ad5d014a3fc605859da423da1eea0ea1
Devlin <devlincodes@gmail.com>
2021-10-28 09:36:44 -0700

    Added new styling and new container

--- src/components/styles/Container.styled.ts
+++ src/components/styles/Container.styled.ts
@@ -4,8 +4,20 @@ export const Container = styled.div`
     display: flex;
     flex-direction: column;
     justify-content: center;
+    flex-wrap: wrap;
     align-items: center;
     width: 100%;
     height: auto;
     text-align: center;
+    gap: 64px;
+
+    img {
+        height: 35vh;
+        width: auto;
+        padding: 32px;
+    }
+`
+
+export const ContainerRow = styled(Container)`
+    flex-direction: row;
 `
