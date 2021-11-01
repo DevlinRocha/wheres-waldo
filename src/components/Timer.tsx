6cda688a9bf67319ded466b06e65394ad05f50f1
Devlin <devlincodes@gmail.com>
2021-11-01 00:42:13 -0700

    Removed unused import, added semantic code

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react"
+import { useEffect } from "react"
 
 interface TimerProps {
     time: number;
@@ -38,10 +38,10 @@ export default function Timer(props: TimerProps) {
     }, [props.isTimerOn]);
 
     return (
-        <div>
+        <time>
             <span>{("0"+Math.floor((props.time/60000)%60)).slice(-2)}:</span>
             <span>{("0"+Math.floor((props.time/1000)%60)).slice(-2)}:</span>
             <span>{("0"+((props.time/10)%100)).slice(-2)}</span>
-        </div>
+        </time>
     );
 };
