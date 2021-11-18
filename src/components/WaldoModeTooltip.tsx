f20c1dbd934f9b6db14c4b5d2c349a44a41624d3
Devlin <devlincodes@gmail.com>
2021-11-17 20:31:11 -0800

    Emphasized differences between difficulties

--- src/components/WaldoModeTooltip.tsx
+++ src/components/WaldoModeTooltip.tsx
@@ -10,13 +10,14 @@ export default function WaldoModeTooltip(props: WaldoModeTooltipProps) {
     <Tooltip isTooltipOpen={props.isTooltipOpen} waldoMode={props.waldoMode}>
       {props.waldoMode ? (
         <p>
-          When "Waldo Mode" is enabled you only need to find Waldo to complete
-          the selected level!
+          When "Waldo Mode" is enabled you <b>only need to find Waldo</b> to
+          complete the selected level!
         </p>
       ) : (
         <p>
-          When "Challenge Mode" is enabled you need to find every hidden
-          character to complete the selected level!
+          When "Challenge Mode" is enabled you{' '}
+          <b>need to find every hidden character</b> to complete the selected
+          level!
         </p>
       )}
     </Tooltip>
