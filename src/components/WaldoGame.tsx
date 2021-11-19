2af99a0367d7498735a18750f25b6d2c2774a28f
Devlin <devlincodes@gmail.com>
2021-11-18 16:00:54 -0800

    Added Woof to resetCharacters function

--- src/components/WaldoGame.tsx
+++ src/components/WaldoGame.tsx
@@ -112,6 +112,7 @@ export default function WaldoGame(props: GameProps) {
     setIsWaldoFound(false);
     setIsWendaFound(false);
     setIsWizardFound(false);
+    setIsWoofFound(false);
   }
 
   async function getCharacters() {
